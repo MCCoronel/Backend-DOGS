2325abd3b1de79d03f9179a2675677f6f8ff7144
mccoronel <mcelestecrnl@gmail.com>
2023-07-18 00:45:05 -0300

    Commit Lunes 16

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./app-server");
 const { db } = require("./src/models");
 const { controllerTemperaments } = require("./src/controllers");
 
-db.sync({force: false}).then(() => {
+db.sync({force: true}).then(() => {
   controllerTemperaments.chargeTemperamentsToDB().then(() => {
     server.listen(3001, () => {
       console.log("%s listening at 3001"); // eslint-disable-line no-console
